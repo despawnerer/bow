87f149bc8f0af65e3f1b4a8cacd1adee0aa3e535
Aleksei Voronov <despawn@gmail.com>
2016-05-23 22:23:34 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='bow',
+    version='0.1',
+    description='Manager isolated environments for your projects using Docker',
+    url='https://github.com/despawnerer/bow',
+    author='Aleksei Voronov',
+    author_email='despawn@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    scripts=['bow'],
+    install_requires=[
+        'click>=6',
+        'docker-py>=1.8',
+        'dockerpty>=0.4',
+    ]
+)
